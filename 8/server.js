85dbfa3b228d963923c43ba05620552919be31bc
yonie <yonie@yonie.org>
2020-12-08 22:33:07 +0100

    Add files via upload

---
+++ 8/server.js
@@ -0,0 +1,68 @@
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('8/input')
+    //input: require('fs').createReadStream('8/test')
+});
+
+var code = []
+
+lineReader.on('line', function (line) {
+
+    code.push(line)
+
+})
+
+lineReader.on('close', function () {
+
+    console.log("code lines in file:", code.length)
+
+    console.log("running code into loop to find accumulator value..")
+    runCode(code,true)
+
+    for (var n=0;n<code.length;n++) {
+
+        // make a new copy
+        var copiedCode = [...code]
+
+        // replace one line
+        if (copiedCode[n].substring(0,3)=='nop') copiedCode[n] = "jmp " + copiedCode[n].split(' ')[1]
+        else if (copiedCode[n].substring(0,3)=='jmp') copiedCode[n] = "nop " + copiedCode[n].split(' ')[1]
+
+        // run the new version
+        var result = runCode(copiedCode)
+        if (result > -1) console.log("answer2",result)
+    }
+
+})
+
+function runCode(programCode,debug) {
+
+    var linesRun = []
+    var accumulator = 0
+
+    for (var cursor=0;cursor<programCode.length;cursor++) {
+
+        // if we run into the same line again, we are in a loop so stop
+        if (linesRun[cursor]) {
+            if (debug) console.log("loop detected. accumulator value was",accumulator)
+            break
+        }
+
+        // save our run code
+        linesRun[cursor] = true
+
+        // parse 
+        var operation = programCode[cursor].substring(0, 3)
+        var argument = Number(programCode[cursor].split(' ')[1])
+
+        // run code
+        if (operation == 'acc') accumulator += argument
+        if (operation == 'jmp') cursor += argument-1
+        if (operation == 'nop') continue
+
+    }
+
+    // exit with accumulator value if we exit the program successfully, otherwise -1
+    if (cursor == code.length) return accumulator
+    else return -1
+
+}

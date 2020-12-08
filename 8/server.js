0ea86d8b479e58888c33e86904381557de4446f0
yonie <yonie@yonie.org>
2020-12-08 22:46:48 +0100

    Add files via upload

--- 8/server.js
+++ 8/server.js
@@ -18,37 +18,37 @@ lineReader.on('close', function () {
     // -- PART ONE --
 
     console.log("running code into loop to find accumulator value..")
-    runCode(code,true)
+    runCode(code, true)
 
     // -- PART TWO --
 
     // we will now modify the code single line every time
-    for (var n=0;n<code.length;n++) {
+    for (var n = 0; n < code.length; n++) {
 
         // make a new copy of the code
         var copiedCode = [...code]
 
         // replace one line in the copy
-        if (copiedCode[n].substring(0,3)=='nop') copiedCode[n] = "jmp " + copiedCode[n].split(' ')[1]
-        else if (copiedCode[n].substring(0,3)=='jmp') copiedCode[n] = "nop " + copiedCode[n].split(' ')[1]
+        if (copiedCode[n].substring(0, 3) == 'nop') copiedCode[n] = "jmp " + copiedCode[n].split(' ')[1]
+        else if (copiedCode[n].substring(0, 3) == 'jmp') copiedCode[n] = "nop " + copiedCode[n].split(' ')[1]
 
         // run the new copy and see if it ran
         var result = runCode(copiedCode)
-        if (result > -1) console.log("answer2",result)
+        if (result > -1) console.log("answer2", result)
     }
 
 })
 
-function runCode(programCode,debug) {
+function runCode(programCode, debug) {
 
     var linesRun = []
     var accumulator = 0
 
-    for (var cursor=0;cursor<programCode.length;cursor++) {
+    for (var cursor = 0; cursor < programCode.length; cursor++) {
 
         // if we run into the same line again, we are in a loop so stop
         if (linesRun[cursor]) {
-            if (debug) console.log("loop detected. accumulator value was",accumulator)
+            if (debug) console.log("loop detected. accumulator value was", accumulator)
             break
         }
 
@@ -61,7 +61,7 @@ function runCode(programCode,debug) {
 
         // run code
         if (operation == 'acc') accumulator += argument
-        if (operation == 'jmp') cursor += argument-1
+        if (operation == 'jmp') cursor += argument - 1
         if (operation == 'nop') continue
 
     }
